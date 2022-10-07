2a9a5fac514f303fd4442f416b0ef237e0298b1c
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-07 11:21:36 +0200

    added navigation molecule

---
+++ src/components/molecules/Navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+
+interface Props {
+    children: React.ReactNode
+}
+
+const Navigation = ({children}: Props) => {
+    return(
+        <nav className="navigation">
+            {children}
+        </nav>
+    )
+}
+
+export default Navigation;
