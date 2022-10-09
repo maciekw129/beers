2215fa468a713298c850b4ba9a0c4d4e0d4778fc
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-09 18:59:03 +0200

    created footer molecule

---
+++ src/components/molecules/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+import Text from '../../atoms/Text/Text';
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <Text 
+                textContent="Copyright - all rights reserved."
+            />
+        </footer>
+    )
+};
+
+export default Footer;
