7ff9794335af3566d04ef45b33e30ebc58cef19a
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-09 19:01:23 +0200

    added footer and beer list to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 // COMPONENTS
 import Header from './components/organisms/Header/Header';
 import Home from './components/templates/Home/Home';
+import BeerList from './components/templates/BeerList/BeerList';
+import Footer from './components/molecules/Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path='*' element={ <Navigate to='/'/> }/>
-        <Route path='/' element={ <Home /> }/>
-      </Routes>
+        <Routes>
+          <Route path='*' element={ <Navigate to='/'/> }/>
+          <Route path='/' element={ <Home /> }/>
+          <Route path='/beer-list' element={ <BeerList />}/>
+        </Routes>
+      <Footer />
     </>
   );
 }
