747a3020ea141612b634cd26818eb926b8d55b5d
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-09 18:58:01 +0200

    created hero organism

---
+++ src/components/organisms/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+// COMPONENTS
+import Search from '../../molecules/Search/Search';
+
+const Hero = () => {
+    return(
+        <div className="hero">
+            <Search />
+        </div>
+    )
+};
+
+export default Hero;
