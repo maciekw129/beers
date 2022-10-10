8de268c19321c079e9e6d0eec6480cd33da8f771
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-10 19:09:01 +0200

    added pagination bar

--- src/components/templates/BeerList/BeerList.tsx
+++ src/components/templates/BeerList/BeerList.tsx
@@ -1,11 +1,13 @@
 import './style.css';
 // COMPONENTS
 import BeerGrid from '../../organisms/BeerGrid/BeerGrid';
+import PaginationBar from '../../molecules/PaginationBar/PaginationBar';
 
 const BeerList = () => {
     return(
         <div className="beerList fixed-full-height">
             <BeerGrid />
+            <PaginationBar />
         </div>
     )
 }
