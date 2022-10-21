0d3a99cd69784da409df0e9bceb533b02021c557
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-21 15:15:40 +0200

    upadated requests

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import Beer from '../types/Beer';
 
-const API: string = 'https://api.punkapi.com/v2/';
+const API: string = 'https://api.punkapi.com/v2/beers';
 
 const requests = {
     getBeersByPage(page: number) {
-        return axios.get<Beer[]>(API + 'beers?page=' + page);
+        return axios.get<Beer[]>(API + '?page=' + page);
     },
     getBeerById(id: number) {
-        return axios.get<Beer[]>(API + 'beers/' + id);
+        return axios.get<Beer[]>(API + '/' + id);
+    },
+    getBeersByName(name: string) {
+        return axios.get<Beer[]>(API + `?beer_name=${name}`);
     }
 };
 
