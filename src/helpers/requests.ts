e79dbeefece9a05ab2d6ac5c40e77bd020ae9573
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-21 11:59:31 +0200

    added request methods

--- src/helpers/requests.ts
+++ src/helpers/requests.ts
@@ -6,6 +6,9 @@ const API: string = 'https://api.punkapi.com/v2/';
 const requests = {
     getBeersByPage(page: number) {
         return axios.get<Beer[]>(API + 'beers?page=' + page);
+    },
+    getBeerById(id: number) {
+        return axios.get<Beer[]>(API + 'beers/' + id);
     }
 };
 
